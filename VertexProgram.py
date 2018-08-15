01e573969ee3f488b698cbc4ed67021e4f4d3c4a
Jannik Vogel <email@jannikvogel.de>
2018-08-15 18:35:28 +0200

    VP RDI dumping

--- VertexProgram.py
+++ VertexProgram.py
@@ -26,7 +26,7 @@ ilu_operations = (
   'lit', # 0x7
 )
 
-output_registers = (
+output_o_registers = (
   'oPos', # 0x0
   None,   # 0x1
   None,   # 0x2
@@ -50,9 +50,9 @@ def get_write_mask(mask):
   return mask_str
 
 
-def get_output_register(register, mask):
+def get_output_o_register(register, mask):
   if register & 0: # FIXME: check if this is in reg space
-    register_str = output_registers[index]
+    register_str = output_o_registers[index]
   else:
     register_str = "c[%d]" % (index - 96)
   register_str += get_write_mask(mask)
@@ -70,10 +70,16 @@ def get_swizzle_mask(mask):
     return "." + c[x]
   return "." + c[x] + c[y] + c[z] + c[w]
 
-def get_input_register(negate, mask):
+def get_input_register(v, c_index, v_index):
+
+  source = get_bits(v, 2)
+  index = get_bits(v, 4)
+  mask = get_bits(v, 8)
+  negate = get_bits(v, 1)
+
   register_str = "-" if negate else ""
   if source == 1: # Register
-    register_str += "r%d" % r_index
+    register_str += "r%d" % index
   elif source == 2: # Vertex attribute
     register_str += "v%d" % v_index
   elif source == 3: # Constant
@@ -86,10 +92,27 @@ def get_input_register(negate, mask):
 
 def get_instruction(instruction):
 
-  mac = 0 # FIXME: Parse
-  ilu = 0 # FIXME: Parse
+  cin = (instruction[0] >> 1) & 1
+  om = (instruction[0] >> 2) & 1
+  output_o_index = (instruction[0] >> 3) & 0x1FF
+  output_o_mask = (instruction[0] >> 12) & 0xF
+  swm = (instruction[0] >> 16) & 0xF
+  output_r_index = (instruction[0] >> 20) & 0xF
+  output_r_mask = (instruction[0] >> 24) & 0xF
+  clo = (instruction[0] >> 28) & 0xF
 
-  owm = False # FIXME: Parse
+  chi = (instruction[1] >> 0) & 0x3FF
+  b = (instruction[1] >> 11) & 0x7FFF
+  alo = (instruction[1] >> 26) & 0x3F
+
+  ahi = (instruction[2] >> 0) & 0x1FF
+  v_index = (instruction[2] >> 9) & 0xF
+  c_index = (instruction[2] >> 13) & 0xF
+  mac = (instruction[2] >> 21) & 0xF
+  ilu = (instruction[2] >> 25) & 0x7F
+
+  c = (chi << 4) | clo
+  a = (ahi << 6) | alo
 
   operations = []
 
@@ -98,21 +121,22 @@ def get_instruction(instruction):
     mac_operation = mac_operations[mac]
 
     mac_str = mac_operation[0]
-    has_output = owm and not om
+    has_o_output = output_o_mask and not om
 
     #FIXME: Is this handled correctly for ARL?
-    two_destinations = has_output and rwm
+    two_destinations = has_o_output and output_r_mask
 
     if two_destinations:
       mac_str += "{ "
 
     if mac == 0xD: # Check for ARL output
+      assert(output_r_mask == 0x0) #FIXME: What would happen here?
       mac_str += "a0.x"
-    elif has_output:
-      mac_str += get_output_register()
+    elif has_o_output:
+      mac_str += get_output_o_register(output_o_index, output_o_mask)
 
     if two_destinations:
-      mac_str += ", r%d%s }" % (rw, rwm)
+      mac_str += ", r%d%s }" % (output_r_index, output_r_mask)
 
     if mac_operation[1]:
       mac_str += ", " + get_input_register(a)
@@ -126,7 +150,7 @@ def get_instruction(instruction):
   if ilu != 0x00:
     ilu_str = ilu_operations[ilu]
 
-    has_output = owm and om
+    has_output = output_o_mask and om
 
     #FIXME: Add output
     #FIXME: Parse arguments
